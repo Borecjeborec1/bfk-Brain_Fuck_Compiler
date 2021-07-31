f727d944b4ff0f8d36d81be19bcc92d454c31e0a
borecjeborec1 <robingrundel@seznam.cz>
2021-07-31 22:12:05 +0200

    added user input

--- src/compile.js
+++ src/compile.js
@@ -1,6 +1,21 @@
 const fs = require('fs');
 const colors = require('colors');
 const path = require('path');
+var readline = require('readline-sync');
+
+async function getFile(bfFileName) {
+  try {
+    return fs.readFileSync(bfFileName, 'utf-8');
+  } catch (er) {
+    return [
+      er
+        .toString()
+        .match(/(?<=open ').+\'/)
+        .toString()
+        .replace("'", ''),
+    ];
+  }
+}
 
 exports.compile = async function (file, cmd) {
   try {
@@ -14,10 +29,12 @@ exports.compile = async function (file, cmd) {
       console.log();
       console.log(`USAGE`.bold.white);
       console.log(`>  bfk <path to your file>`);
+      console.log(`>  bfk <path to your file> -str`);
       console.log();
       console.log(`COMMANDS`.bold.white);
       console.log(`>  -help   Display help for BFK`);
       console.log(`>  -v      Display version of BFK`);
+      console.log(`>  -str    Returns compiled file as continous string`);
       //AD NEW COMMANDS
       return;
     }
@@ -27,7 +44,14 @@ exports.compile = async function (file, cmd) {
     }
     let toStr = cmd == '-str';
     const bfFileName = file.includes('.bf') ? file : file + '.bf';
-    let program = await fs.readFileSync(bfFileName, 'utf-8');
+    let program = await getFile(bfFileName);
+    if (typeof program == 'object') {
+      console.log();
+      console.log(`Unsuccessfully Compiled!`.red.bold);
+      console.log(`> Your result:`, `No such file or directory`.cyan, `${bfFileName}`.bold.cyan);
+      console.log(`> What to do: `, `Change the path to the file. You're probably misspelled!`.cyan);
+      return;
+    }
     let block = Array(100).fill(0);
     let pointer = 0;
     let isLooping = false;
@@ -44,12 +68,7 @@ exports.compile = async function (file, cmd) {
         }
         continue;
       }
-      if (resString && i == program.length - 1) {
-        console.log(resString);
-      } else if (i == program.length - 1) {
-        console.log();
-        console.log('> Thank you for using', 'BFK!'.bold.cyan);
-      }
+
       switch (program[i]) {
         case '+':
           block[pointer]++;
@@ -58,6 +77,8 @@ exports.compile = async function (file, cmd) {
           block[pointer]--;
           break;
         case ',':
+          var t = readline.question('Write: ');
+          block[pointer] = t.charCodeAt();
           break;
         case '.':
           if (toStr) {
@@ -82,6 +103,14 @@ exports.compile = async function (file, cmd) {
           break;
         default:
           break;
+      }
+      if (resString && i == program.length - 1) {
+        console.log(resString);
+        console.log();
+        console.log('> Thank you for using', 'BFK!'.bold.cyan);
+      } else if (i == program.length - 1) {
+        console.log();
+        console.log('> Thank you for using', 'BFK!'.bold.cyan);
       }
     }
   } catch (e) {
